e7107f93bc578d8b178e6337508e70e786958892
snowchik <ihor.v.milinchuk@ukd.edu.ua>
2025-03-20 18:29:08 +0200

    Налаштування маршрутизації

--- expense/apps.py
+++ expense/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class ExpenceConfig(AppConfig):
+class ExpenseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'expence'
+    name = 'expense'
