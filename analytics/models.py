5e2950b02d29eae2175d4bca8d2f09cdc6cdf4f9
snowchik <ihor.v.milinchuk@ukd.edu.ua>
2025-03-20 18:10:09 +0200

    Створення моделі

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Report(models.Model):
+    REPORT_TYPES = [
+        ('income_expense', 'Доходи та витрати'),
+        ('goal_progress', 'Прогрес цілей'),
+        ('comparison', 'Порівняння періодів'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    report_type = models.CharField(max_length=20, choices=REPORT_TYPES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    data = models.JSONField() 
+
+    def __str__(self):
+        return f"{self.get_report_type_display()} ({self.created_at.date()})"
