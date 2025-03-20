9821da08f91b170ad78f3dffced5025501508981
snowchik <ihor.v.milinchuk@ukd.edu.ua>
2025-03-20 18:24:41 +0200

    Створення логіки витрат

--- expence/views.py
+++ expence/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Expense
+from .forms import ExpenseForm
 
-# Create your views here.
+@login_required
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            return redirect('expenses_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', {'form': form})
+
+@login_required
+def expenses_list(request):
+    expenses = Expense.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'expenses/expenses_list.html', {'expenses': expenses})
